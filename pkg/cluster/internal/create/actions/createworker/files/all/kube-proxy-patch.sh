d5aca6c9f4e5377f683d7169e3b07421bd1975d1
Francisco Orselli <forselli@stratio.com>
2023-06-26 09:49:15 +0200

    Expose kube-proxy metrics

---
+++ pkg/cluster/internal/create/actions/createworker/files/all/kube-proxy-patch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#
+# I couldn't find a better/simpler way to conifgure it. See:
+# https://github.com/kubernetes-sigs/cluster-api/issues/4512
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly dir="/run/kubeadm"
+
+# Run this script only if this is the init node.
+if [[ ! -f ${dir}/kubeadm.yaml ]]; then
+    exit 0
+fi
+
+# Exit fast if already appended.
+if [[ ! -f ${dir}/kube-proxy-config.yaml ]]; then
+    exit 0
+fi
+
+cat ${dir}/kube-proxy-config.yaml >> ${dir}/kubeadm.yaml
+rm ${dir}/kube-proxy-config.yaml
